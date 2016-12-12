f771d48f3d38af040c95bafcf04ebabf6c4aaf1b
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-12 11:43:16 -0500

    Initial commit

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, title, storyline, poster, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster
+        self.trailer_youtube_url = trailer
