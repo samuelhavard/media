3e07c6d243078e114dca88864453d5038908c725
Samuel Havard <SamuelHavard@yahoo.com>
2016-12-13 06:54:48 -0500

    update

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,4 +4,21 @@ toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
-print (toy_story.storyline)
+# print (toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+#print (avatar.storyline)
+# avatar.show_trailer()
+
+american_history_x = media.Movie("American History X",
+                                 "A brutal Neo Nazi skinhead named Derek Vinyard is tried and sent to prison for "
+                                 "three years for the murder of two black guys who tried to steal his truck. When he "
+                                 "returns from prison reformed, his younger brother Daniel Vinyard who idolizes him "
+                                 "is on the brink of becoming a Neo Nazi himself.",
+                                 "https://en.wikipedia.org/wiki/American_History_X#/media/File:"
+                                 "American_history_x_poster.jpg",
+                                 "https://www.youtube.com/watch?v=JsPW6Fj3BUI")
+#american_history_x.show_trailer()
--- media.py
+++ media.py
@@ -1,6 +1,12 @@
+import webbrowser
+
+
 class Movie():
     def __init__(self, title, storyline, poster, trailer):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster
         self.trailer_youtube_url = trailer
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
